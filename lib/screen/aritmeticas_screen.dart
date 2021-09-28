e1f49b7dbdda1c67ff06494c369627adbe72bb3c
neiderSteven <rodriguezneiderstiven@gmail.com>
2021-09-28 11:27:47 -0500

    operaciones aritmeticas y logicas

--- lib/screen/aritmeticas_screen.dart
+++ lib/screen/aritmeticas_screen.dart
@@ -10,10 +10,97 @@ class AritmeticasSreen extends StatefulWidget {
 class _AritmeticasSreenState extends State<AritmeticasSreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('operaciones aritmeticas'),
-      ),
+    String binary1 = '';
+    String binary2 = '';
+
+    return myForm(binary1, binary2, context);
+  }
+
+  Form myForm(String binary1, String binary2, BuildContext context) {
+    return Form(
+        autovalidateMode: AutovalidateMode.disabled,
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              onChanged: (binario1) {
+                binary1 = binario1;
+              },
+              decoration: InputDecoration(
+                  labelText: 'Escriba el numero binario',
+                  prefixIcon: Icon(Icons.format_list_numbered)),
+            ),
+            TextFormField(
+              autocorrect: false,
+              onChanged: (binario2) {
+                binary2 = binario2;
+              },
+              decoration: InputDecoration(
+                  labelText: 'Escriba el numero binario',
+                  prefixIcon: Icon(Icons.format_list_numbered)),
+            ),
+            SizedBox(height: 15),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 0,
+                color: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text('Sumar'),
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => _convertir('suma', binary1, binary2),
+                  );
+                }),
+            SizedBox(height: 15),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 0,
+                color: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text('Restar'),
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => _convertir('resta', binary1, binary2),
+                  );
+                }),
+          ],
+        ));
+  }
+
+  Widget _convertir(numero, binary1, binary2) {
+    var valor;
+
+    if (numero == 'suma') {
+      var testC = int.parse(binary1, radix: 2) + int.parse(binary2, radix: 2);
+      valor = BigInt.from(testC).toUnsigned(8).toRadixString(2);
+    }
+    if (numero == 'resta') {
+      var testC = int.parse(binary1, radix: 2) - int.parse(binary2, radix: 2);
+      valor = BigInt.from(testC).toUnsigned(8).toRadixString(2);
+    }
+
+    return myAlert(valor);
+  }
+
+  AlertDialog myAlert(valor) {
+    return AlertDialog(
+      title: Text('Resultado'),
+      content: Text("Valor: ${valor}"),
+      actions: <Widget>[
+        TextButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            }),
+      ],
     );
   }
 }
--- lib/screen/logicas_screen.dart
+++ lib/screen/logicas_screen.dart
@@ -10,10 +10,117 @@ class LogicaScreen extends StatefulWidget {
 class _LogicaScreenState extends State<LogicaScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('operadores logicos'),
-      ),
+    String binary1 = '';
+    String binary2 = '';
+
+    return myForm(binary1, binary2, context);
+  }
+
+  Form myForm(String binary1, String binary2, BuildContext context) {
+    return Form(
+        autovalidateMode: AutovalidateMode.disabled,
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              onChanged: (binario1) {
+                binary1 = binario1;
+              },
+              decoration: InputDecoration(
+                  labelText: 'Escriba el numero binario',
+                  prefixIcon: Icon(Icons.format_list_numbered)),
+            ),
+            TextFormField(
+              autocorrect: false,
+              onChanged: (binario2) {
+                binary2 = binario2;
+              },
+              decoration: InputDecoration(
+                  labelText: 'Escriba el numero binario',
+                  prefixIcon: Icon(Icons.format_list_numbered)),
+            ),
+            SizedBox(height: 15),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 0,
+                color: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text('AND'),
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => _convertir('and', binary1, binary2),
+                  );
+                }),
+            SizedBox(height: 15),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 0,
+                color: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text('OR'),
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => _convertir('or', binary1, binary2),
+                  );
+                }),
+            SizedBox(height: 15),
+            MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                elevation: 0,
+                color: Colors.blue,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: Text('XOR'),
+                ),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (_) => _convertir('xor', binary1, binary2),
+                  );
+                })
+          ],
+        ));
+  }
+
+    Widget _convertir(numero, binary1, binary2) {
+    var valor;
+
+    if (numero == 'and') {
+      var testC = int.parse(binary1, radix: 2) & int.parse(binary2, radix: 2);
+      valor = BigInt.from(testC).toUnsigned(8).toRadixString(2);
+    }
+    if (numero == 'or') {
+      var testC = int.parse(binary1, radix: 2) | int.parse(binary2, radix: 2);
+      valor = BigInt.from(testC).toUnsigned(8).toRadixString(2);
+    }
+    if (numero == 'xor') {
+      var testC = int.parse(binary1, radix: 2) ^ int.parse(binary2, radix: 2);
+      valor = BigInt.from(testC).toUnsigned(8).toRadixString(2);
+    }
+
+    return myAlert(valor);
+  }
+
+  AlertDialog myAlert(valor) {
+    return AlertDialog(
+      title: Text('Resultado'),
+      content: Text("Valor: ${valor}"),
+      actions: <Widget>[
+        TextButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            }),
+      ],
     );
   }
 }
